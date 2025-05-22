82813168cbcbe61694b067aada1e4254fa9da412
ChiaJung Chung <heavenly0430@gmail.com>
2025-05-22 17:17:36 +0200

    update

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
-  has_many :movies, dependent: :destroy
-  has_many :lists, dependent: :destroy
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
